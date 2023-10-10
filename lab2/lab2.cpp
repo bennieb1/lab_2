833584dfb75c3212dbe37fd06d7a0aea349df108
Brandon Bennie <bennieb@shernet.sheridancollege.ca>
2023-10-10 16:52:22 -0400

    working json parsing

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -2,12 +2,19 @@
 #include <fstream>
 #include <iostream>
 #include "json.hpp"
-
+#include "SDL.h"
+#include "SDL_image.h"
 #include "Engine.h"
 
 
 int main(int argc, char* args[])
 {
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	window = SDL_CreateWindow("SDL Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 	
 
